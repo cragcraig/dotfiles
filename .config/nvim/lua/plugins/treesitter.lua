1d31a20cce90d5e029c7cd591ca62e1a73f3bb07
craigdh <craigdh@google.com>
2026-01-16 16:18:20 -0700

    Fix nvim treesitter plugin config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,40 +1,42 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate',
-
-  config = function()
-    require('nvim-treesitter').install({
-      'go',
-      'rust',
-      'json',
-      'html',
-      'css',
-      'markdown',
-      'regex',
-      'python',
-      'lua',
-      'vim',
-      'vimdoc',
-      'c',
-    })
-
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = {
-        'go',
-        'rust',
-        'python',
-        'c',
-        'vim',
-        'lua',
-        'markdown',
-        'json',
-        'html',
-      },
-      callback = function()
-        vim.treesitter.start()
-      end,
-    })
+  "nvim-treesitter/nvim-treesitter",
+  -- Instruct lazy.nvim to run the TSUpdate command after installation/updates
+  build = ":TSUpdate",
+  -- Pass options to the setup function
+  opts = {
+    -- Automatically install parsers
+    auto_install = true,
+    -- List of languages to ensure are installed
+    ensure_installed = {
+      "awk",
+      "bash",
+      "c",
+      "cpp",
+      "diff",
+      "dot",
+      "fish",
+      "go",
+      "html",
+      "kotlin",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "proto",
+      "python",
+      "query",
+      "rust",
+      "vim",
+      "vimdoc",
+    },
+    -- Enable specific features
+    highlight = { enable = true }, -- Enable syntax highlighting
+    indent = { enable = true },    -- Enable smart code indentation
+    folds = { enable = true },      -- Enable folding support
+  },
+  -- Manually call the setup function with the options provided in 'opts'
+  config = function(_, opts)
+    require("nvim-treesitter.config").setup(opts)
   end,
 }
